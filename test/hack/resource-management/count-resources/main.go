aadf510aa4e5c41355e8335403c92aa1c8a18ad9
Amanuel Engeda <amanueng@gmail.com>
2023-11-15 10:18:38 -0800

    Add Resource count to Timestream

--- test/hack/resource-management/count-resources/main.go
+++ test/hack/resource-management/count-resources/main.go
@@ -17,8 +17,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
-	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
@@ -31,32 +29,19 @@ import (
 	"github.com/aws/karpenter/test/hack/cleanup/resourcetypes"
 )
 
-const expirationTTL = time.Hour * 12
+const karpenterMetricTableName = "resourceCount"
 
 func main() {
-	var clusterName string
-	if len(os.Args) == 2 {
-		clusterName = os.Args[1]
-	}
 	ctx := context.Background()
 	cfg := lo.Must(config.LoadDefaultConfig(ctx))
 
 	logger := lo.Must(zap.NewProduction()).Sugar()
 
-	expirationTime := time.Now().Add(-expirationTTL)
-
-	logger.With("expiration-time", expirationTime.String()).Infof("resolved expiration time for all resourceTypes")
-
 	ec2Client := ec2.NewFromConfig(cfg)
 	cloudFormationClient := cloudformation.NewFromConfig(cfg)
 	iamClient := iam.NewFromConfig(cfg)
-
 	metricsClient := metrics.Client(metrics.NewTimeStream(cfg))
 
-	// These resources are intentionally ordered so that instances that are using ENIs
-	// will be cleaned before ENIs are attempted to be cleaned up. Likewise, instances and ENIs
-	// are cleaned up before security groups are cleaned up to ensure that everything is detached and doesn't
-	// prevent deletion
 	resourceTypes := []resourcetypes.Type{
 		resourcetypes.NewInstance(ec2Client),
 		resourcetypes.NewVPCEndpoint(ec2Client),
@@ -70,26 +55,14 @@ func main() {
 
 	for i := range resourceTypes {
 		resourceLogger := logger.With("type", resourceTypes[i].String())
-		var ids []string
-		var err error
-		if clusterName == "" {
-			ids, err = resourceTypes[i].GetExpired(ctx, expirationTime)
-		} else {
-			ids, err = resourceTypes[i].Get(ctx, clusterName)
-		}
+		resourceCount, err := resourceTypes[i].GetCount(ctx)
 		if err != nil {
 			resourceLogger.Errorf("%v", err)
 		}
-		resourceLogger.With("ids", ids, "count", len(ids)).Infof("discovered resourceTypes")
-		if len(ids) > 0 {
-			cleaned, err := resourceTypes[i].Cleanup(ctx, ids)
-			if err != nil {
-				resourceLogger.Errorf("%v", err)
-			}
-			if err = metricsClient.FireMetric(ctx, fmt.Sprintf("%sDeleted", resourceTypes[i].String()), float64(len(cleaned)), cfg.Region); err != nil {
-				resourceLogger.Errorf("%v", err)
-			}
-			resourceLogger.With("ids", cleaned, "count", len(cleaned)).Infof("deleted resourceTypes")
+
+		if err = metricsClient.FireMetric(ctx, karpenterMetricTableName, fmt.Sprintf("%s", resourceTypes[i].String()), float64(resourceCount), cfg.Region); err != nil {
+			resourceLogger.Errorf("%v", err)
 		}
+		resourceLogger.With("count", resourceCount).Infof("counted resourceTypes")
 	}
 }
