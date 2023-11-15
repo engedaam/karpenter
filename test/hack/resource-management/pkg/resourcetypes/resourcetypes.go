046878e46046dd155c2d4b7c61b797277c9fd182
Amanuel Engeda <amanueng@gmail.com>
2023-11-15 10:18:38 -0800

    Add Resource count to Timestream

--- test/hack/resource-management/pkg/resourcetypes/resourcetypes.go
+++ test/hack/resource-management/pkg/resourcetypes/resourcetypes.go
@@ -42,4 +42,6 @@ type Type interface {
 	// In general, if all resources can't be deleted by id with a single API call (like with DeleteInstances)
 	// you should call the requests synchronously to avoid rate limiting against the number of requests made
 	Cleanup(ctx context.Context, ids []string) (cleaned []string, err error)
+	// GetCount returns the number of resource of the type in the account
+	GetCount(ctx context.Context) (count int, err error)
 }
