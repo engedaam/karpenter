1af272145cbe58bf5176d07d9e991f934aff6bdb
Amanuel Engeda <aengeda@amazon.com>
2024-04-11 17:52:43 -0700

    Create a Providers controller

---
+++ pkg/controllers/providers/instancetype/controller.go
@@ -0,0 +1,48 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package instancetypes
+
+import (
+	"context"
+
+	awscache "github.com/aws/karpenter-provider-aws/pkg/cache"
+	"github.com/aws/karpenter-provider-aws/pkg/providers/instancetype"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/karpenter/pkg/operator/controller"
+)
+
+type Controller struct {
+	instancetypeProvider instancetype.Provider
+}
+
+func NewController(instancetypeProvider instancetype.Provider) *Controller {
+	return &Controller{
+		instancetypeProvider: instancetypeProvider,
+	}
+}
+
+func (c *Controller) Reconcile(ctx context.Context, _ reconcile.Request) (reconcile.Result, error) {
+
+	return reconcile.Result{RequeueAfter: awscache.InstanceTypesAndZonesTTL}, nil
+}
+
+func (c *Controller) Name() string {
+	return "instancetype"
+}
+
+func (c *Controller) Builder(_ context.Context, m manager.Manager) controller.Builder {
+	return controller.NewSingletonManagedBy(m)
+}
